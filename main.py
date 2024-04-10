5002dfa9ef22f24628399027ee37f0acd596f09d
akash20-06 <akashkmr1216@gmail.com>
2024-04-11 00:15:43 +0530

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import torchvision.transforms as transforms
 # Load the model
 model_path = 'model.pkl'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = torch.load(model_path, map_location=device)
+model = torch.load(model_path, map_location=torch.device('cpu'))
 model.eval()
 
 class_names = ['MildDemented', 'ModerateDemented', 'NonDemented', 'VeryMildDemented']
